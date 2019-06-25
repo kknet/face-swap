01764f186b1f5a91efa5661c205d0a462a6d06ee
Oriol Bernal <obernalpo@gmail.com>
2019-06-25 22:42:41 +0200

    last steps

--- example3.py
+++ example3.py
@@ -1,5 +1,5 @@
 import cv2
-from face_swap import get_triangles_img, show_process
+from face_swap import get_triangles_img, get_process
 from image_utils import concatenate_images, resize_and_border_image, create_gif
 
 
@@ -22,13 +22,13 @@ img2 = cv2.imread(imgPath2)
 # Resize and border image, so both images have same dimensions
 img, img2 = resize_and_border_image(img, img2)
 
-first_steps = show_process(img, img2)
+steps = get_process(img, img2)
 
 # Concatenate images
 first_steps = concatenate_images(
     (
-        concatenate_images(first_steps[0], axis='h', border=5),
-        concatenate_images(first_steps[1], axis='h', border=5)
+        concatenate_images(steps[0], axis='h', border=5),
+        concatenate_images(steps[1], axis='h', border=5)
     ),
     axis='v',
     border=0
@@ -39,5 +39,19 @@ cv2.imwrite('results/first_steps81.png', first_steps)
 
 second_step = create_gif('results/second_step81.gif', append_triangles_img(img, img2))
 
-cv2.imshow("result", first_steps)
+# Concatenate images
+last_steps = concatenate_images(
+    (
+        concatenate_images(steps[2], axis='h', border=5),
+        concatenate_images(steps[3], axis='h', border=5)
+    ),
+    axis='v',
+    border=0
+)
+last_steps = cv2.resize(last_steps, (int(last_steps.shape[1]/3), int(last_steps.shape[0]/3)))
+cv2.imwrite('results/last_steps81.png', first_steps)
+
+
+cv2.imshow("first_steps", first_steps)
+cv2.imshow("last_steps", last_steps)
 cv2.waitKey(0)
